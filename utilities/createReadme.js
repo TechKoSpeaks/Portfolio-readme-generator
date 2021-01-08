be9050fc6de033c4a8c4653891d4f6aac3df7435
Travis Ko <tkospeaks@gmail.com>
2021-01-08 15:51:39 -0800

    Added in readme.js into utilities folder for adding and generating the readme into the readwrite folder. (for creating the readme.md) Added in module exports for creating the readme

---
+++ utilities/createReadme.js
@@ -0,0 +1,45 @@
+// Creating a Readme with the function populating the README.md
+function createReadme(answers) {
+    return `
+<h1 align="center">${answers.projectName}</h1>
+  
+
+## Description
+🔍 ${answers.describe}
+
+## Table of Contents
+- [Description](#description)
+- [Installation](#instruct)
+- [Usage](#usageInfo)
+- [Guidelines](#guidelines)
+- [Testing](#instructions)
+- [License](#license)
+
+## Installation
+${answers.instruct}
+
+## Usage Information
+${answers.usageInfo}
+
+## Contribution Guidelines
+${answers.guidelines}
+
+## Test Instructions
+${answers.guidelines}
+
+## License
+${answers.license}-brightblue)
+<br />
+This application is under the ${answers.license} license. 
+
+
+<br />
+Find me on GitHub: [${answers.githubUser}](https://github.com/${answers.githubUser})<br />
+<br />
+
+Email me with any questions: ${answers.emailAddress}<br /><br />
+
+    `;
+  }
+  
+  module.exports = createReadme;
